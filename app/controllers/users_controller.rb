326c945dbbbdb5b6bedf11979fdeba9c2688fde1
Ashley Wong <ashwong04@gmail.com>
2019-03-19 21:08:38 +0000

    checked if logged_in? in login and sign up, added logout route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,17 +19,30 @@ class UsersController < ApplicationController
   end
 
   get '/login' do
-    erb :"users/login"
+    if logged_in?
+      redirect '/tickets/index'
+    else
+      erb :"users/login"
+    end
   end
 
   post '/login' do
-      @user = User.find_by(:username => params[:username], :password => params[:password])
-      if @user
-        reidrect '/tickets/index'
-      else
-        redirect '/login'
-      end
+    @user = User.find_by(:username => params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect '/tickets/index'
+    else
+      redirect '/login'
     end
+  end
 
+  get '/logout' do
+    if logged_in?
+      session.clear
+      redirect to '/login'
+    else
+      redirect to '/'
+    end
+  end
 
 end
