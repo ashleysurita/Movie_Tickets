d7c5fe9e031ab56df8b485ca6c8318a286e72188
Ashley Wong <ashwong04@gmail.com>
2019-03-19 20:59:29 +0000

    added logged_in? and current_user helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,14 @@ class ApplicationController < Sinatra::Base
     erb :welcome
   end
 
+  helpers do
+     def logged_in?
+       !!session[:user_id]
+     end
+
+    def current_user
+       User.find(session[:user_id])
+     end
+   end
+   
 end
