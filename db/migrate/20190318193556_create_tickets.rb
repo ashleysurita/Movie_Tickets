2529f2f7395f2f8a8083e44c15305e5f09de3c59
Ashley Wong <ashwong04@gmail.com>
2019-03-19 21:15:56 +0000

    created migration tables

--- db/migrate/20190318193556_create_tickets.rb
+++ db/migrate/20190318193556_create_tickets.rb
@@ -1,7 +1,10 @@
 class CreateTickets < ActiveRecord::Migration
   def change
     create_table :tickets do |t|
-
+      t.string :movie_name
+      t.date :date
+      t.string :movie_theater
+      t.integer :user_id
       t.timestamps null: false
     end
   end
