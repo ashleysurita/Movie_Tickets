4343b25158086dd8e4c3117bef4ca5e8648ee50b
Ashley Wong <ashwong04@gmail.com>
2019-03-20 01:14:18 +0000

    removed slug methods, will use id instead

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,14 +1,4 @@
 class Ticket < ActiveRecord::Base
   belongs_to :user
 
-  def slug
-    self.username.downcase.gsub(" ", "-")
-  end
-
-  def self.find_by_slug(slug)
-    song = self.all.find do |s|
-      s.slug == slug
-    end
-  end
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,4 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :tickets
 
-  def slug
-    self.username.downcase.gsub(" ", "-")
-  end
-
-  def self.find_by_slug(slug)
-    song = self.all.find do |s|
-      s.slug == slug
-    end
-  end
-  
 end
