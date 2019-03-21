bd72b8255afe122e99c6dab27be295e7aa1cf4e0
Ashley Wong <ashwong04@gmail.com>
2019-03-21 13:50:32 +0000

    changed / route to check if logged in, and logs user out if true

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,11 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/" do
-    erb :welcome
+    if logged_in?
+      redirect '/logout'
+    else
+      erb :welcome
+    end
   end
 
   helpers do
